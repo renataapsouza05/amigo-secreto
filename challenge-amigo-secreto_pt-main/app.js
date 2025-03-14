33fb28d6d345792b8711a8f33906a174ad5d4f9d
Renata A Souza <renataapsouza05@gmail.com>
2025-03-13 22:12:37 -0300

    Projeto quase finalizado com diversas melhorias implementadas

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeNomes = [];
+
+function adicionarAmigo() {
+    let amigo = document.querySelector('input').value;
+
+    if  (amigo === "") {
+        alert('Por favor, insira um nome.');
+    } else {
+        listaDeNomes.push(amigo);
+        exibirListaDeNomes();
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    amigo = document.querySelector('input');
+    amigo.value = '';
+}
+
+function exibirListaDeNomes() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let nome of listaDeNomes) {
+        let itemLista = document.createElement('li');
+        itemLista.textContent = nome;
+        lista.appendChild(itemLista);
+    }
+        
+    }
+
